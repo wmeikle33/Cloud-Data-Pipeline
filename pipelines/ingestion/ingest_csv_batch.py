ebe4524869a452c944cf1807e221be84599677ae
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-04-20 07:53:47 +0800

    Update ingest_csv_batch.py

--- pipelines/ingestion/ingest_csv_batch.py
+++ pipelines/ingestion/ingest_csv_batch.py
@@ -3,7 +3,9 @@ def extract_from_source():
         print(f"Reading file: {file}")
         df = pd.read_csv(file)
         
-
+def basic_cleaning():
+    duplicate_count = df.duplicated().sum()
+    
 def run():
     data = extract_from_source()
     data = basic_cleaning(data)
