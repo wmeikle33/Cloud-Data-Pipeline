58d11e066baaa1b989e4260caaf15b9b953c497f
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-04-19 08:46:33 +0800

    Create training_dag.py

---
+++ orchestration/airflow/dags/training_dag.py
@@ -0,0 +1,27 @@
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+from pipelines.training_data.build_dataset import run as build_data
+from pipelines.model.train import run as train_model
+
+
+with DAG(
+    dag_id="ml_training_pipeline",
+    start_date=datetime(2024, 1, 1),
+    schedule_interval="@daily",
+    catchup=False,
+) as dag:
+
+    build_dataset_task = PythonOperator(
+        task_id="build_dataset",
+        python_callable=build_data,
+    )
+
+    train_task = PythonOperator(
+        task_id="train_model",
+        python_callable=train_model,
+    )
+
+    build_dataset_task >> train_task
